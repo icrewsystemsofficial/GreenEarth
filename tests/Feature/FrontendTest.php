02f33ec0ad5ffdb1a055c5118d8e6433c7df5506
Leonard Selvaraja <kashrayks@gmail.com>
2021-10-06 04:23:09 +0530

    Added PEST

---
+++ tests/Feature/FrontendTest.php
@@ -0,0 +1,40 @@
+<?php
+/*
+    GreenEarth Test Cases
+
+    THIS APPLICATION USES PHP PEST!
+    1. The file name should end in Test. eg: XYZTest.php
+    2. All tests should have a clearly defined @objective & @expected
+*/
+
+
+    uses()->group('frontend');
+
+    /*  Test # 1
+        @objective: Checks if the "/" route is redirected to the home.index
+        @expected: 302
+    */
+
+    test('Test#1: If root page redirects to home route', function () {
+        $this->get('/')->assertStatus(302);
+    });
+
+    /*  Test # 2
+        @objective: Checks if "home" route renders
+        @expected: 200
+    */
+
+    test('Test#2: If home route renders', function () {
+        $this->get('/home')->assertStatus(200);
+    });
+
+
+
+    // it('has homepage', function () {
+    //     $this->browse(function (Browser $browser) {
+    //         $browser->visit('/')
+    //             ->assertSee('Pest');
+    //     });
+    // });
+
+?>
