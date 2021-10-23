ef8238b8f456bc0fca6aaa1e7a48b198040d79d5
Thirumalai15 <m.thirurahul@gmail.com>
2021-10-23 23:41:10 +0530

    controller class name fixed in web.php file

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,7 @@ use App\Http\Controllers\Portal\Admin\UserController;
 use App\Http\Controllers\Portal\DirectoriesController;
 use App\Http\Controllers\FAQController;
 use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\PlantSpecieController;
+use App\Http\Controllers\PlantSpeciesController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Http\Controllers\Portal\Admin\AnnouncementController;
 use App\Http\Controllers\Portal\Admin\ContactRequestController;
