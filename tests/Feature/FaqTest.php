d9208e27005f614455d679cc66b45a91fac94f0e
Harshit Garg <harshitgarg2001217@gmail.com>
2021-10-25 20:26:16 +0530

    Faq test updated

--- tests/Feature/FaqTest.php
+++ tests/Feature/FaqTest.php
@@ -14,12 +14,17 @@ beforeEach(function () {
     $this->withoutExceptionHandling();
 });
 
-test('Test#1: Check if Portal(Non-admin) FAQ Page renders', function () {
+test('Test#1: Check if Frontend FAQ Page renders', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user)->get('/home/faq')->assertStatus(200);
+});
+
+test('Test#2: Check if Portal(Non-admin) FAQ Page renders', function () {
     $user = User::factory()->create();
     $this->actingAs($user)->get('/portal/faq')->assertStatus(200);
 });
 
-test('Test#2: Check if you can view a FAQ individually from Portal(Non-admin) FAQ Page', function () {
+test('Test#3: Check if you can view a FAQ individually from Portal(Non-admin) FAQ Page', function () {
     $id = random_int(1, 5);
     $faq = FAQ::factory()->create();
     $slug = Str::slug($faq->title, '-');
@@ -28,17 +33,17 @@ test('Test#2: Check if you can view a FAQ individually from Portal(Non-admin) FA
     $this->actingAs($user)->get('/portal/faq/' . $slug)->assertStatus(200);
 });
 
-test('Test#3: Check if Portal(Admin) FAQ Page renders', function () {
+test('Test#4: Check if Portal(Admin) FAQ Page renders', function () {
     $user = User::factory()->create();
     $this->actingAs($user)->get('/portal/admin/faq')->assertStatus(200);
 });
 
-test('Test#4: Check if create new FAQ page renders', function () {
+test('Test#5: Check if create new FAQ page renders', function () {
     $user = User::factory()->create();
     $this->actingAs($user)->get('/portal/admin/faq/create')->assertStatus(200);
 });
 
-test('Test#5: Check if Admin is able to add a new FAQ', function () {
+test('Test#6: Check if Admin is able to add a new FAQ', function () {
     $user = User::factory()->create();
     $faq = FAQ::factory()->create();
     $this->actingAs($user)->post(route('portal.admin.faq.store'),  array(
@@ -49,13 +54,13 @@ test('Test#5: Check if Admin is able to add a new FAQ', function () {
     ))->assertStatus(302);
 });
 
-test('Test#6: Check if manage FAQ page renders', function () {
+test('Test#7: Check if manage FAQ page renders', function () {
     $user = User::factory()->create();
     $faq = FAQ::factory()->create();
     $this->actingAs($user)->get('/portal/admin/faq/edit/' . $faq->id)->assertStatus(200);
 });
 
-test('Test#7: Check if Admin is able to edit a FAQ', function () {
+test('Test#8: Check if Admin is able to edit a FAQ', function () {
     $user = User::factory()->create();
     $faq = FAQ::factory()->create();
     $this->actingAs($user)->post(route('portal.admin.faq.update', $faq->id),  array(
@@ -65,7 +70,7 @@ test('Test#7: Check if Admin is able to edit a FAQ', function () {
     ))->assertStatus(302);
 });
 
-test('Test#8: Check if Admin is able to delete a FAQ', function () {
+test('Test#9: Check if Admin is able to delete a FAQ', function () {
     $user = User::factory()->create();
     $faq = FAQ::factory()->create();
     $this->actingAs($user)->get('/portal/admin/faq/delete/' . $faq->id)->assertStatus(302);
