7eba29a31b566d30b3f8e705eeff0714d15c03de
johanth <johanth6600@gmail.com>
2021-10-25 21:18:31 +0530

    clerup commit

--- resources/views/pages/tree/index.blade.php
+++ resources/views/pages/tree/index.blade.php
@@ -100,54 +100,7 @@
                         </tbody>
                     </table>
                 </div>
-<<<<<<< HEAD
             </div>
-=======
-                <table id="all-trees" class="table text-center table-responsive">
-                    <thead>
-                        <tr>
-                            <th> TREE ID  </th>
-                            <th> TREE PLANTED ON </th>
-                            <th> LAST MAINTAINED </th>
-                            <th> HEALTH </th>
-                            <th> ACTIONS </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($trees as $tree)
-                        <tr style="vertical-align: middle;">
-                            <td> {{ $tree->id }} </td>
-                            <td> {{ $tree->created_at->diffForHumans() }} </td>
-                            <td> @if($tree->last_maintained)
-                                {{ \Carbon\Carbon::parse($tree->last_maintained)->diffForHumans() }} 
-                                @endif
-                            </td>
-                            <td> 
-                                @if($tree->health == "Healthy")
-                                <span class="badge bg-success text-white">
-                                {{ $tree->health }}
-                                </span> 
-                                @elseif($tree->health == "Not So Healthy")
-                                <span class="badge bg-warning text-white">
-                                {{ $tree->health }}
-                                </span>
-                                @else
-                                <span class="badge bg-danger text-white">
-                                {{ $tree->health }}
-                                </span>
-                                @endif
-                            </td>
-                            <td class="pb-0">
-                                <a href="{{route('portal.admin.tree.manage',$tree->id)}}" class="btn btn-info btn-sm">
-                                    Manage
-                                </a>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>  
->>>>>>> 1558bd33c2814bea7d8a0b49730fabb742e5b584
         </div>
     </div>
 @endsection
