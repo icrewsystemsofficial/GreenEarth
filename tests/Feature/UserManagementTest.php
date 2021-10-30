63f4cf7481b0c89e4a08d008addd5ac19e2b52bc
aren01 <hafizmutalib01@gmail.com>
2021-10-30 16:10:10 +0800

    most tests done, whats left is validation and also email test

--- tests/Feature/UserManagementTest.php
+++ tests/Feature/UserManagementTest.php
@@ -2,65 +2,110 @@
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\User;
+use Spatie\Permission\Models\Role;
 
 uses()->group('users');
+uses(RefreshDatabase::class);
+
 
 beforeEach(function () {
     $this->withoutExceptionHandling();
+    $this->user = User::factory()->create();
+
+    $this->actingAs($this->user);
+
+
+    $this->id = $this->user->id;
+    // $this->name = $user->name;
+    // $this->email = $user->email;
+    // $this->role = $user->role;
+
 });
 
 test('Test #1: has usermanagement page', function () {
-    $user = User::factory()->create();
 
-    $this->actingAs($user)->post(route('portal.admin.users.add_user'),  array(
-        'name' => $user->name,
-        'email' => $user->email,
-        'password' => $user->password,
-        'role' => $user->role,
-
-    ))->assertStatus(302);
-
+    $this->get(route('portal.admin.users.create'))->assertStatus(200);
 });
 
 test('Test #2: has createuser page', function () {
-    $user = User::factory()->create();
 
-    $this->actingAs($user)->get(route('portal.admin.users.create'))->assertStatus(200);
+    $this->get(route('portal.admin.users.create'))->assertStatus(200);
 });
 
 test('Test #3: has modifyuser page', function () {
-    $user = User::factory()->create();
 
-    $this->actingAs($user)->get(route('portal.admin.users.manage' . $user->id))->assertStatus(200);
+    $this->get('/portal/admin/users/manage/' . $this->id)->assertStatus(200);
 });
 
 test('Test#4: Check if the new user is stored properly', function () {
-    $user = User::factory()->create();
-    $this->actingAs($user)->post(route('portal.admin.users.add_user'),  array(
-        'name' => $user->name,
-        'email' => $user->email,
-        'password' => $user->password,
+    $this->withoutExceptionHandling();
 
-    ))->assertStatus(302);
+    $role = Role::create(['name' => 'user']);
+
+    $data = [
+        'name' => 'test',
+        'email' => 'test@gmail.com',
+        'password' => 'password',
+        'role' => $role['name'],
+    ];
+
+    $this->post(route('portal.admin.users.store', $data))
+        ->assertRedirect(route('portal.admin.users.index'));
 });
 
-test('Test#5: Check if the new user is stored properly without required fields', function () {
-    $user = User::factory()->create();
-    $this->actingAs($user)->post(route('portal.admin.users.add_user'),  array(
-        //missing field - name
-        'email' => $user->email,
-        'password' => $user->password,
+test('Test#5: Check if the new user is getting updated', function () {
 
-    ))->assertStatus(302);
+    $role = Role::create(['name' => 'user']);
+
+    $data = [
+        'name' => 'test',
+        'email' => 'test@gmail.com',
+        'password' => 'password',
+        'role' => $role['name'],
+    ];
+
+    $newuser = User::create($data);
+
+    $data['name'] = 'new name';
+
+    $this->from(route('portal.admin.users.manage', $newuser))
+        ->post(route('portal.admin.users.update', $newuser->id), $data)
+        ->assertStatus(302);
+
+    $this->assertDatabaseHas('users', [
+        'id' => $newuser->id,
+        'name' => 'new name'
+    ]);
 });
 
-test('Test#6: Check if the new user is stored properly with empty model', function () {
-    $user = User::factory()->create();
-    $this->actingAs($user)->post(route('portal.admin.users.add_user'),  array(
-        'name' => $user->name,
-        'email' => $user->email,
-        'password' => $user->password,
+test('Test#6: Check if the new user is getting deleted', function () {
 
-    ))->assertStatus(302);
+    $role = Role::create(['name' => 'user']);
+
+    $data = [
+        'name' => 'test',
+        'email' => 'test@gmail.com',
+        'password' => 'password',
+        'role' => $role['name'],
+    ];
+
+    $newuser = User::create($data);
+
+    $this->from(route('portal.admin.users.manage', $newuser))
+        ->post(route('portal.admin.users.delete', $newuser->id), $data)
+        ->assertStatus(302);
+
+    $this->assertDatabaseMissing('users', [
+        'id' => $newuser->id,
+        'name' => 'new name'
+    ]);
 });
 
+test('Test#7: Check for validation', function () {
+
+    $this->$this->post('/portal/admin/users/update/' . $this->id,  array(
+            'name' => '',
+            'email' => '',
+
+        ))->assertStatus(302);
+})->skip();
